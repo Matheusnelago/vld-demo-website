09c7e9c1fad9797987ba00fe096cb83031cb3220
Matheus Nelago <130694676+Matheusnelago@users.noreply.github.com>
2025-04-11 01:46:34 +0200

    Delete libs directory

--- libs/components/script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu-icon')
-const navlinks = document.querySelector('.nav-links')
-menuBtn.addEventListener('click', () => {
-    navlinks.classList.toggle('mobile-menu')
-    menuBtn.display = '/libs/icons/menu-close.svg'
-})
